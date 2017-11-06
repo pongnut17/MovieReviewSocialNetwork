a3128de2a5723b32e8b9898a3055b520475d21d5
Pongnut Jittipanyakul <pongnut17@hotmail.com>
2017-11-06 15:00:48 +0700

    Add some instance and method to User class

--- src/User.java
+++ src/User.java
@@ -1,3 +1,40 @@
+import java.util.ArrayList;
+
 public class User
 {
+    private String Email;
+    private String Password;
+    private String Username;
+    private ArrayList<String> favoriteGenre;
+    private UserCollection follower;
+    private UserCollection following;
+    private ReviewCollection ownReview;
+
+    public User(String Email, String Password, String Username)
+    {
+        this.Email = Email;
+        this.Password = Password;
+        this.Username = Username;
+    }
+
+    public String getEmail()
+    {
+        return Email;
+    }
+    public String getUsername()
+    {
+        return Username;
+    }
+    public ArrayList<String> getFavoriteGenre()
+    {
+        return favoriteGenre;
+    }
+    public void addFavoriteGenre(String genre)
+    {
+        favoriteGenre.add(genre);
+    }
+    public boolean removeFavoriteGenre(String genre)
+    {
+        return favoriteGenre.remove(genre);
+    }
 }
